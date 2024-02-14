8f5519f7a6a5cf8d8f026ebbef8be4d41db34b4c
StavrosCharalampidis <stavroscharalampidis1@gmail.com>
2024-02-14 03:22:00 +0200

    (Try The input module) Complete

---
+++ ex11.py
@@ -0,0 +1,26 @@
+# Print the comment for input
+print("Type your name?", end=' ')
+
+# The Variable is for input a name 
+name = input()
+
+# Print the comment for input
+print("Type your age?", end=' ')
+
+# The Variable is for input a age 
+age = int(input())
+
+# Print the comment for input
+print("Type your weigh?",end=' ')
+
+# The Variable is for input a weigh
+weigh = input()
+
+# Print the comment for input
+print("Type your favorite namber?",end=' ')
+
+# The Variable is for input a favNum
+favNum = int(input())
+
+# Print all the input variables
+print(f'Hello {name} yor age is {age} and your weigh is {weigh} and yor favorite num minus your age is {favNum - age}')
